b914e979bc68d46eaaccc8c12eaa52d97822b640
thelastpride27 <ashton.magri@students.mq.edu.au>
2025-05-06 12:52:05 +1000

    unity project stuff

---
+++ Assets/Tiled/TilemapGame/TinyDungeon_Tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeon_Tilesheet" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+ <image source="../kenney_tiny-dungeon/Tilemap/tilemap.png" width="203" height="186"/>
+</tileset>
